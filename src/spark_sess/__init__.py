91fe241cd84d072d5c4455892991755d9bbfe148
imranmaj <49664304+imranmaj@users.noreply.github.com>
2024-01-08 11:14:34 -0800

    Add files

---
+++ src/spark_sess/__init__.py
@@ -0,0 +1,20 @@
+from pyspark.sql import SparkSession
+
+spark = (
+    SparkSession.builder.appName("parquet_converter")
+    .master("local[*]")
+    .config(
+        "spark.jars.packages",
+        "org.apache.hadoop:hadoop-aws:3.2.2,org.postgresql:postgresql:42.6.0",
+    )
+    .config(
+        "fs.s3a.aws.credentials.provider",
+        "com.amazonaws.auth.DefaultAWSCredentialsProviderChain",
+    )
+    .config("spark.hadoop.fs.s3a.impl.disable.cache", True)
+    .config("spark.hadoop.fs.s3a.path.style.access", True)
+    .config("spark.driver.memory", "10g")
+    .config("spark.driver.memoryOverhead", "10g")
+    .config("spark.executor.memoryOverhead", "10g")
+    .getOrCreate()
+)
